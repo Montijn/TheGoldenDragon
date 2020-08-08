5399b8366728ef5be67abb7eca0da71418dfb655
Montijn <ms.bennik@student.avans.nl>
2020-08-08 21:17:19 +0200

    small cleanup

---
+++ DeGoudenDraak/blog/resources/views/partials/mainpartial.blade.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html>
+<head>
+    <title>The Golden Dragon</title>
+    <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
+</head>
+
+<body>
+<table class="main_table">
+    <tr class="main_tr" >
+        <td class="td_image"  >
+            <img src="pictures/dragon-small.png" alt="Golden Dragon" height="50px">
+            <span >De Gouden Draak</span>
+            <img src="pictures/dragon-small-flipped.png" alt="Golden Dragon" height="50px">
+        </td>
+        <td>
+            <a href="paginas/aanbiedingen.html" id="sidescroller">
+                <div class="marquee">
+                    <p>Welkom bij De Gouden Draak. Klik op deze tekst om de aanbiedingen van deze week te zien!</p>
+                </div>
+            </a>
+        </td>
+        <td class="td_image" >
+            <img src="pictures/dragon-small.png" alt="Golden Dragon" height="50px">
+            <span>De Gouden Draak</span>
+            <img src="pictures/dragon-small-flipped.png" alt="Golden Dragon" height="50px">
+        </td>
+    </tr>
+</table>
+<table class="main_table">
+    <tr class="border_tr">
+        <td colspan="9">
+        </td>
+    <tr>
+    <tr class="basic_tr" >
+        <td width="7px">
+        </td>
+        <td class="left_top_td" ></td>
+        <td class="right_top_td" ></td>
+        <td class="right_bottom_td" ></td>
+        <td class="top_bottom_td"></td>
+        <td class="left_bottom_td" ></td>
+        <td class="left_top_td"></td>
+        <td class="right_top_td" ></td>
+        <td width="7px">
+    </tr>
+    <tr class="basic_tr" >
+        <td width="7px">
+        <td class="left_bottom_td" ></td>
+        <td class="basic_td"></td>
+        <td class="basic_td"></td>
+        <td></td>
+        <td class="basic_td"></td>
+        <td class="basic_td"></td>
+        <td class="right_bottom_td"></td>
+        <td width="7px">
+    </tr>
+    <tr class="basic_tr" >
+        <td width="7px">
+        <td class="right_bottom_td" ></td>
+        <td class="basic_td"></td>
+        <td class="width_td"></td>
+        <td></td>
+        <td class="width_td"></td>
+        <td class="basic_td"></td>
+        <td class="bottom_td" ></td>
+        <td width="7px">
+    </tr>
+    <tr class="main_tr">
+        <td width="7px">
+        <td class="right_left_td"></td>
+        <td class="width_td"></td>
+        <td class="width_td"></td>
+        <td style="text-align:center">
+            <!-- CONTENT HERE! -->
+            <table width=100%>
+                <tr>
+                    <td colspan='3'>
+                        <p>
+                            <img src="pictures/dragon-small.png" style="float:left;height:200px" alt="Golden Dragon">
+                            <img src="pictures/dragon-small-flipped.png" style="float:right;height:200px" alt="Golden Dragon">
+                            <span style="font-size:40px;font-weight:bold;color:yellow">Chinees Indische Specialiteiten</span><br>
+                            <span style="font-size:50px;font-weight:bold;color:yellow">De Gouden Draak</span><br>
+                        </p>
+                        <p>
+                        <table style="margin:auto;font-size:20px;color:white" border="1px solid white">
+                            <tr background="pictures/menu_bg_gradient.png">
+                                <td valign="middle">
+                                    <a href="paginas/MENUKAART.html" style="color:white">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Menukaart&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    </a>
+                                </td>
+                                <td valign="middle">
+                                    <a href="paginas/news.html" style="color:white">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nieuws&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    </a>
+                                </td>
+                                <td valign="middle">
+                                    <a href="paginas/contact.html" style="color:white">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    </a>
+                                </td>
+                            </tr>
+                        </table>
+                        </p>
+                    </td>
+                </tr>
+                <tr style="padding-top:50px">
+                    <td colspan="3" height="50px">
+                    </td>
+                </tr>
+            </table>
+            <br>
+            <div text-align="center"><a href="paginas/contact_new.html">Naar Contact</a></div>
+        </td>
+        <td class="width_td"></td>
+        <td class="width_td"></td>
+        <td class="right_left_td"></td>
+        <td width="7px">
+    </tr>
+    <tr class="basic_tr" >
+        <td width="7px">
+        <td class="right_top_td" ></td>
+        <td class="basic_td"></td>
+        <td class="width_td"></td>
+        <td></td>
+        <td class="width_td"></td>
+        <td class="basic_td"></td>
+        <td class="left_top_td"></td>
+        <td width="7px">
+    </tr>
+    <tr class="basic_tr" >
+        <td width="7px">
+        <td class="left_top_td"></td>
+        <td class="basic_td"></td>
+        <td class="basic_td"></td>
+        <td></td>
+        <td class="basic_td"></td>
+        <td class="basic_td"></td>
+        <td class="right_top_td" ></td>
+        <td width="7px">
+    </tr>
+    <tr class="basic_tr" >
+        <td width="7px">
+        </td>
+        <td class="left_bottom_td" ></td>
+        <td class="right_bottom_td" ></td>
+        <td class="right_td"></td>
+        <td class="top_bottom_td"></td>
+        <td class="left_td" ></td>
+        <td class="left_bottom_td" ></td>
+        <td class="right_bottom_td" ></td>
+        <td width="7px">
+    </tr>
+    <tr class="border_tr">
+        <td colspan="9">
+        </td>
+    <tr>
+</table>
+</body>
+</html>
