8c1039579136ed8a46b2a798772486fec17264f8
MitchVerweerden <49711991+MitchVerweerden@users.noreply.github.com>
2020-08-09 11:59:58 +0200

    Controllers + models added

---
+++ DeGoudenDraak/blog/app/Http/Controllers/RiceChoiceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RiceChoice;
+use Illuminate\Http\Request;
+
+class RiceChoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RiceChoice  $riceChoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RiceChoice $riceChoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RiceChoice  $riceChoice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RiceChoice $riceChoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RiceChoice  $riceChoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RiceChoice $riceChoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RiceChoice  $riceChoice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RiceChoice $riceChoice)
+    {
+        //
+    }
+}
---
+++ DeGoudenDraak/blog/app/Http/Controllers/SpecialOffersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SpecialOffer;
+use Illuminate\Http\Request;
+
+class SpecialOffersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SpecialOffer  $specialOffer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SpecialOffer $specialOffer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SpecialOffer  $specialOffer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SpecialOffer $specialOffer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SpecialOffer  $specialOffer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SpecialOffer $specialOffer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SpecialOffer  $specialOffer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SpecialOffer $specialOffer)
+    {
+        //
+    }
+}
