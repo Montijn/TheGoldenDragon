8660a00a011ff2c57b1d0dc00f9787a54ffed471
MitchVerweerden <mverweer@avans.nl>
2023-05-18 13:35:19 +0200

    import script fix

--- gouden_draak_import.sql
+++ gouden_draak_import.sql
@@ -1,17 +1,18 @@
 START TRANSACTION;
+SET SQL_SAFE_UPDATES = 0;
 
 -- ADD TEMPORARY COLUMN    
 ALTER TABLE menu_items
 ADD COLUMN dishtype_name VARCHAR(45) AFTER dish_type;
 
--- INSERT MENU ITEMS TO DISHES (CHANGE THE FROM VALUE TO CORRECT DB NAME)
-INSERT INTO golden_dragon.menu_items (menu_code, menu_code_addition, name, price, description, dishtype_name)
-SELECT menunummer, menu_toevoeging, naam, price, beschrijving, soortgerecht
-FROM gouden_draak.menu;
-
 -- INSERT DISH CATEGORIES
 INSERT INTO golden_dragon.dish_types (name)
 SELECT DISTINCT soortgerecht
+FROM gouden_draak.menu;
+
+-- INSERT MENU ITEMS TO DISHES (CHANGE THE FROM VALUE TO CORRECT DB NAME)
+INSERT INTO golden_dragon.menu_items (menu_code, menu_code_addition, name, price, description, dishtype_name, dish_type)
+SELECT menunummer, menu_toevoeging, naam, price, beschrijving, soortgerecht, 1
 FROM gouden_draak.menu;
 
 -- INSERT CORRECT CATEGORY ID IN DISH TABLE
@@ -26,4 +27,5 @@ WHERE menu_items.id IS NOT NULL;
 
 ALTER TABLE golden_dragon.menu_items DROP dishtype_name;
 
+SET SQL_SAFE_UPDATES = 1;
 COMMIT;
