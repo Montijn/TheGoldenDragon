043ce3dbfc814616f0dcecf824cf896d6765442c
MitchVerweerden <mverweer@avans.nl>
2023-08-22 14:09:09 +0200

    styling template

---
+++ DeGoudenDraak/laravel/resources/views/website/homepage.blade.php
@@ -0,0 +1,9 @@
+@extends('website.layouts.website-layout')
+
+@section('content')
+    <div class="row my-3">
+        <div class="col-lg-8 flex-">
+           
+        </div>
+    </div>
+@endsection
